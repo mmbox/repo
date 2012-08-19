02fd2efde71eea268a5082064582c4225df6059c
mmbox <mmbox.me@gmail.com>
2012-08-19 12:25:27 +0800

    add hello.c

---
+++ hello.c
@@ -0,0 +1,22 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hello.c
+ *
+ *    Description:  just print hello world
+ *
+ *        Version:  1.0
+ *        Created:  2012/08/19 12时22分59秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  mmbox 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+
+main() {
+	printf("hello world\n");
+}
